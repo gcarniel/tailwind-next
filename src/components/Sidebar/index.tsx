252935725dcbd7864f6b5a2a2fef0697c860f184
Gabriel Carniel <gpcarniel@outlook.com>
2023-10-28 16:49:36 -0300

    aplicando responsividade na sidebar

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,4 +1,6 @@
-import { Cog, LifeBuoy, Search } from 'lucide-react'
+'use client'
+
+import { Cog, LifeBuoy, Menu, Search } from 'lucide-react'
 import { Logo } from './Logo'
 import {
   BarChart,
@@ -12,40 +14,59 @@ import { NavItem } from './NavItem'
 import { UsedSpaceWidget } from './UsedSpaceWidget'
 import Profile from './Profile'
 import { InputControl, InputPrefix, InputRoot } from '../Input'
+import * as Collpasible from '@radix-ui/react-collapsible'
+import { Button } from '../Button'
 
 export function Sidebar() {
   return (
-    <aside className="border-r border-zinc-200 px-5 py-8 flex flex-col gap-6">
-      <Logo />
+    <Collpasible.Root
+      className={`border-r border-zinc-200 p-4 flex flex-col gap-6 
+                fixed top-0 left-0 right-0 data-[state=open]:bottom-0 z-20 bg-white border-b
+                lg:right-auto lg:w-80 lg:border-r lg:py-8 lg:px-5 lg:data-[state=closed]:bottom-0 
+                `}
+    >
+      <div className="flex items-center justify-between">
+        <Logo />
+        <Collpasible.Trigger className="lg:hidden" asChild>
+          <Button variant="ghost">
+            <Menu className="h-5 w-5" />
+          </Button>
+        </Collpasible.Trigger>
+      </div>
 
-      <InputRoot>
-        <InputPrefix>
-          <Search className="h-5 w-5 text-zinc-500" />
-        </InputPrefix>
-        <InputControl placeholder="Search" />
-      </InputRoot>
+      <Collpasible.Content
+        forceMount
+        className="flex flex-1 flex-col gap-6 data-[state=closed]:hidden lg:data-[state=closed]:flex "
+      >
+        <InputRoot>
+          <InputPrefix>
+            <Search className="h-5 w-5 text-zinc-500" />
+          </InputPrefix>
+          <InputControl placeholder="Search" />
+        </InputRoot>
 
-      <nav className="space-y-0.5">
-        <NavItem title="Home" icon={Home} />
-        <NavItem title="Dashboard" icon={BarChart} />
-        <NavItem title="Projects" icon={SquareStack} />
-        <NavItem title="Tasks" icon={CheckSquare} />
-        <NavItem title="Reporting" icon={Flag} />
-        <NavItem title="Users" icon={Users} />
-      </nav>
-
-      <div className="mt-auto flex flex-col gap-6">
-        <nav>
-          <NavItem title="Support" icon={LifeBuoy} />
-          <NavItem title="Settings" icon={Cog} />
+        <nav className="space-y-0.5">
+          <NavItem title="Home" icon={Home} />
+          <NavItem title="Dashboard" icon={BarChart} />
+          <NavItem title="Projects" icon={SquareStack} />
+          <NavItem title="Tasks" icon={CheckSquare} />
+          <NavItem title="Reporting" icon={Flag} />
+          <NavItem title="Users" icon={Users} />
         </nav>
 
-        <UsedSpaceWidget />
+        <div className="mt-auto flex flex-col gap-6">
+          <nav>
+            <NavItem title="Support" icon={LifeBuoy} />
+            <NavItem title="Settings" icon={Cog} />
+          </nav>
 
-        <div className="h-px bg-zinc-200"></div>
+          <UsedSpaceWidget />
 
-        <Profile />
-      </div>
-    </aside>
+          <div className="h-px bg-zinc-200"></div>
+
+          <Profile />
+        </div>
+      </Collpasible.Content>
+    </Collpasible.Root>
   )
 }
